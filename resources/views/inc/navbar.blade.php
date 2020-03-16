354b81238f1534974e03887b8f5b0fd2d8e39ff4
Borisov Roman <rborisov@codemastersintl.com>
2020-03-16 15:33:15 +0300

    * added some views and controllers

---
+++ resources/views/inc/navbar.blade.php
@@ -0,0 +1,16 @@
+<nav class="navbar navbar-dark bg-dark">
+        <a class="navbar-brand" href="/">{{config('app.name','Laravel')}}</a>
+        <div class="navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" href="/">Home</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/about">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/services">Services</a>
+            </li>
+        </ul>
+        </div>
+      </nav>
