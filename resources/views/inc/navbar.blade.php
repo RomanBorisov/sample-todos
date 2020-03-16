4db2deade3865a102526ce12209d94094fef0196
Borisov Roman <rborisov@codemastersintl.com>
2020-03-16 17:28:16 +0300

    * added functionality for creating post

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -14,6 +14,10 @@
             <li class="nav-item">
                 <a class="nav-link" href="/posts">Blog</a>
             </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/posts/create">Create Post</a>
+            </li>
+
         </ul>
     </div>
 </nav>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,13 +4,14 @@
        <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="{{asset('css/app.css')}}">
-        
+
         <title>{{config('app.name','Laravel')}}</title>
 
     </head>
     <body>
         @include('inc.navbar')
         <div class="container">
+            @include('inc.messages')
             @yield('content')
         </div>
     </body>
